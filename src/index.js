59fb16f1f4b6670cd23914dcf8e64e5f5052335a
Sergei Abrasov <intel7@yandex.ru>
2021-03-14 13:45:18 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+	if (Array.isArray(array) && array.length) {
+		return Math.min.apply(this, array);
+	}
+	return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+	if (Array.isArray(array) && array.length) {
+		return Math.max.apply(this, array);
+	}
+	return 0
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+	if (Array.isArray(array) && array.length) {
+		let avgOfArray = 0;
+		let sumOfArray = 0;
+		for (let i = 0; i < array.length; i++) {
+			sumOfArray += array[i];
+		}
+		avgOfArray = sumOfArray/array.length;
+		return avgOfArray;
+	}
+	return 0;
+};
